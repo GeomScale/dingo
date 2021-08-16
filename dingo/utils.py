937becbfa284e660975e8845958a2164b0ac6bdf
hariszaf <haris-zaf@hcmr.gr>
2021-08-16 19:17:10 +0300

    buildConqMatrix function added

--- dingo/utils.py
+++ dingo/utils.py
@@ -198,16 +198,46 @@ def plot_histogram(reaction_fluxes, reaction, n_bins=40):
     plt.show()
 
 
-def matrices_for_community_level(Aeq_list, beq_list, ):
-#     """A Python function to derive the matrices A, Aeq and the vectors b, beq of the low dimensional polytope at the community level,
-#     such that A*x <= b and Aeq*x = beq.
+def buildConqMatrix(list_of_matrices):
 
-#     Keyword arguments:
-#     S -- the stoichiometric matrix
-#     lb -- lower bounds for the fluxes, i.e., a n-dimensional vector
-#     ub -- upper bounds for the fluxes, i.e., a n-dimensional vector
-#     min_fluxes -- minimum values of the fluxes, i.e., a n-dimensional vector
-#     max_fluxes -- maximum values for the fluxes, i.e., a n-dimensional vector
-#     """
+    m      = 0
+    n      = 0
+    frames = [] 
+    for matrix in list_of_matrices: 
+        m += matrix.shape[0]
+        n += matrix.shape[1]
+        frames.append(matrix.shape[1])
 
-    return 'ok'
+    counter = 0
+    parts   = []
+    for matrix in list_of_matrices:
+        if counter == 0: 
+            compl = np.zeros((matrix.shape[0], n - matrix.shape[1]))
+            part  = np.concatenate((matrix, compl), axis=1)
+
+        elif counter == len(list_of_matrices) - 1: 
+            compl = np.zeros((matrix.shape[0], n - matrix.shape[1]))
+            part  = np.concatenate((compl, matrix), axis=1)
+
+        else:
+            prev        = sum(frames[:counter])
+            after       = sum(frames[counter+1:])
+            compl_prev  = np.zeros((matrix.shape[0], prev))
+            compl_after = np.zeros((matrix.shape[0], after))
+            part_a      = np.concatenate((compl_prev, matrix), axis=1)
+            part        = np.concatenate((part_a, compl_after), axis=1)
+                        
+        parts.append(part)
+        counter += 1
+
+    part_counter = 0 
+    for part in parts: 
+        if part_counter == 0: 
+            concMatrix = part                
+        else: 
+            concMatrix = np.concatenate((concMatrix, part), axis=0)
+        part_counter += 1
+
+    return concMatrix
+
+ 
