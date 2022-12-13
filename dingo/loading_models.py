547803290dc81919a76167f18e14947df5069e80
hariszaf <haris.zafeiropoulos@kuleuven.be>
2022-12-13 02:40:46 +0100

    build model from sbml file, issue #26

--- dingo/loading_models.py
+++ dingo/loading_models.py
@@ -9,7 +9,8 @@
 import json
 import scipy.io
 import numpy as np
-
+import cobra
+import pandas as pd
 
 def read_json_file(input_file):
     """A Python function to Read a Bigg json file and returns,
@@ -178,5 +179,71 @@ def read_mat_file(input_file):
     # Get biomass index
     biomass_index = np.where(biomass_function == 1)
     biomass_index = biomass_index[0][0]
+
+    return lb, ub, S, metabolites, reactions, biomass_index, biomass_function
+
+
+def read_sbml_file(input_file):
+    """A Python function, based on the cobra.io.read_sbml_model() function of cabrapy  
+    and the extract_polytope() function of PolyRound 
+    (https://gitlab.com/csb.ethz/PolyRound/-/blob/master/PolyRound/static_classes/parse_sbml_stoichiometry.py)
+    to read an SBML file (.xml) and return:
+    (a) lower/upper flux bounds
+    (b) the stoichiometric matrix S (dense format)
+    (c) the list of the metabolites
+    (d) the list of reactions
+    (e) the index of the biomass pseudoreaction
+    (f) the objective function to maximize the biomass pseudoreaction
+
+    Keyword arguments:
+    input_file -- a xml file that contains an SBML  model with the information about a mettabolic network, for example see: 
+    https://github.com/VirtualMetabolicHuman/AGORA/blob/master/CurrentVersion/AGORA_1_03/AGORA_1_03_sbml/Abiotrophia_defectiva_ATCC_49176.xml
+    """
+    inf_bound=1e5
+    try: 
+        cobra.io.read_sbml_model(  input_file )
+    except:
+        cobra_config = cobra.Configuration()
+        cobra_config.solver = 'glpk'
+
+    model = cobra.io.read_sbml_model( input_file )
+
+    metabolites = [ metabolite.id for metabolite in model.metabolites ]
+    reactions = [ reaction.id for reaction in model.reactions ]
+
+    S = cobra.util.array.create_stoichiometric_matrix(model)
+
+    lb  = []
+    ub = []
+    biomass_function = np.zeros( len(model.reactions) )
+
+    for index, reaction in enumerate(model.reactions):
+
+        if reaction.objective_coefficient==1:
+            biomass_index = index
+            biomass_function[index] = 1
+
+        if reaction.bounds[0] == float("-inf"):
+            lb.append( -inf_bound )
+        else:
+            lb.append( reaction.bounds[0] )
+
+        if reaction.bounds[1] == float("inf"):
+            ub.append( inf_bound )
+        else:
+            ub.append( reaction.bounds[1] )
+
+    lb = np.asarray(lb)
+    ub = np.asarray(ub)
+
+    biomass_function = np.asarray(biomass_function)
+    biomass_function = np.asarray(biomass_function, dtype="float")
+    biomass_function = np.ascontiguousarray(biomass_function, dtype="float")
+
+    lb = np.asarray(lb, dtype="float")
+    lb = np.ascontiguousarray(lb, dtype="float")
+
+    ub = np.asarray(ub, dtype="float")
+    ub = np.ascontiguousarray(ub, dtype="float")
 
     return lb, ub, S, metabolites, reactions, biomass_index, biomass_function
