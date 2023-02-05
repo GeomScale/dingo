cea5cff231d2bb4331f7de10f475b10236b24322
Apostolos Chalkis <apostolos.chalkis@quantagonia.com>
2023-02-05 21:19:41 +0200

    export svg and increase pixels

--- dingo/illustrations.py
+++ dingo/illustrations.py
@@ -8,6 +8,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import plotly.io as pio
 from dingo.utils import compute_copula
 
 def plot_copula(data_flux1, data_flux2, n = 5):
@@ -24,16 +25,30 @@ def plot_copula(data_flux1, data_flux2, n = 5):
     flux1 = data_flux1[0]
     flux2 = data_flux2[0]
     copula = compute_copula(flux1, flux2, n)
-
-    fig = go.Figure(data=[go.Surface(z=copula)])
-
-    fig.update_layout(title='Copula between '+data_flux1[1]+' and '+data_flux2[1], autosize=False,
-                      width=500, height=500, scene = dict(xaxis_title=data_flux1[1],
-                      yaxis_title=data_flux2[1], zaxis_title='prob. mass'),
-                      margin=dict(l=65, r=50, b=65, t=90))
-    
-    fig.update_layout()
+    fig = go.Figure(
+            data   = [go.Surface(z=copula)],
+            layout = go.Layout(
+                height = 600,
+                width  = 900,
+            )
+        )
+    fig.update_layout(
+            title = 'Copula between '+ data_flux1[1] + ' and ' + data_flux2[1],
+            scene = dict(
+                    xaxis_title= data_flux1[1],
+                    yaxis_title= data_flux2[1],
+                    zaxis_title="prob. mass"
+                ),
+            margin=dict(r=30, b=30, l=30, t=50))
+    fig.layout.template = None
     fig.show()
+    fig_name = data_flux1[1] + "_" + data_flux2[1] + "_copula." + "svg"
+    camera = dict(
+        eye=dict(x=2, y=2, z=0.1)
+    )
+    fig.update_layout(scene_camera=camera)
+    fig.to_image(format = "svg", engine="kaleido")
+    pio.write_image(fig, fig_name, scale=2)
 
 
 def plot_histogram(reaction_fluxes, reaction, n_bins=40):
