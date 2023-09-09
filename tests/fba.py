a9e3ddf6946a9c99af60ef3871c43fa9f8b7543f
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-09-09 14:48:12 +0200

    test for boundary change and decreasing of fba when glc goes down

--- tests/fba.py
+++ tests/fba.py
@@ -40,6 +40,26 @@ class TestFba(unittest.TestCase):
 
         self.assertTrue(abs(res[1] - 0.8739215067486387) < 1e-03)
 
+    def test_modify_medium(self):
+
+        input_file_sbml = os.getcwd() + "/ext_data/e_coli_core.xml"
+        model = MetabolicNetwork.from_sbml(input_file_sbml)
+        model.set_slow_mode()
+
+        initial_medium = model.medium
+        initial_fba = model.fba()[-1]
+        glc_index = model.reactions.index("EX_glc__D_e")
+
+        new_media = initial_medium.copy()
+        new_media["EX_glc__D_e"] = 1.5
+
+        model.medium = new_media
+
+        if model.lb[glc_index] != -1.5:
+            self.assertTrue(model.lb[glc_index] == -1.5)
+
+        self.assertTrue(initial_fba - model.fba()[-1] > 0)
+
 
 if __name__ == "__main__":
     unittest.main()
