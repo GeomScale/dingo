19efaef5a90f733834e4be3c867a30c7c9fd37fd
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-10-06 22:35:24 +0530

    Update redundant_facet_removal_experiment.py

--- dingo/experimental_gurobi_functions/redundant_facet_removal_experiment.py
+++ dingo/experimental_gurobi_functions/redundant_facet_removal_experiment.py
@@ -12,53 +12,6 @@ import gurobipy as gp
 from gurobipy import GRB
 import math
 
-
-
-    
-
-    
-
-
-
-
-
-
-def update_model(model, n, Aeq_sparse, beq, lb, ub, A_sparse, b, objective_function):
-    """A function to update a gurobi model that solves a linear program
-    Keyword arguments:
-    model -- gurobi model
-    n -- the dimension
-    Aeq_sparse -- a sparse matrix s.t. Aeq_sparse x = beq
-    beq -- a vector s.t. Aeq_sparse x = beq
-    lb -- lower bounds for the variables, i.e., a n-dimensional vector
-    ub -- upper bounds for the variables, i.e., a n-dimensional vector
-    A_sparse -- a sparse matrix s.t. A_sparse x <= b
-    b -- a vector matrix s.t. A_sparse x <= b
-    objective_function -- the objective function, i.e., a n-dimensional vector
-    """
-    model.remove(model.getVars())
-    model.update()
-    model.remove(model.getConstrs())
-    model.update()
-    x = model.addMVar(
-        shape=n,
-        vtype=GRB.CONTINUOUS,
-        name="x",
-        lb=lb,
-        ub=ub,
-    )
-    model.update()
-    model.addMConstr(Aeq_sparse, x, "=", beq, name="c")
-    model.update()
-    model.addMConstr(A_sparse, x, "<", b, name="d")
-    model.update()
-    model.setMObjective(None, objective_function, 0.0, None, None, x, GRB.MINIMIZE)
-    model.update()
-
-    return model
-
-
-
 def solve_lp_with_different_objectives(model, new_objective_coeffs):
     """
     Solve a linear program with a different objective function.
