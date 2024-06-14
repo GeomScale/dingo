5721c69b6d51f1e50b2a2176e3b65559736cd127
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2024-06-14 15:42:33 +0300

    created PreProcess class

--- dingo/preprocess.py
+++ dingo/preprocess.py
@@ -2,19 +2,24 @@
 import cobra
 from cobra.io import load_json_model
 
+
+class PreProcess:
+    
+    def __init__(self, model):
+        self.model = model
+    
+    def blocked_reactions(model):
+
+        model.objective = 'BIOMASS_Ecoli_core_w_GAM'
+        fba_solution = model.optimize()
+
+        model.reactions.get_by_id("BIOMASS_Ecoli_core_w_GAM").lower_bound = fba_solution.objective_value
+
+        fva = cobra.flux_analysis.flux_variability_analysis(model, fraction_of_optimum=0.95)
+        blocked = cobra.flux_analysis.find_blocked_reactions(model)
+        return blocked
+
+
 model = load_json_model("../ext_data/e_coli_core.json")
-
-
-model.objective = 'BIOMASS_Ecoli_core_w_GAM'
-fba_solution = model.optimize()
-print(fba_solution.objective_value)
-
-
-model.reactions.get_by_id("BIOMASS_Ecoli_core_w_GAM").lower_bound = fba_solution.objective_value
-print(model.reactions.get_by_id("BIOMASS_Ecoli_core_w_GAM").lower_bound)
-print(model.reactions.get_by_id("BIOMASS_Ecoli_core_w_GAM").upper_bound)
-
-
-fva = cobra.flux_analysis.flux_variability_analysis(model, fraction_of_optimum=0.95)
-blocked = cobra.flux_analysis.find_blocked_reactions(model)
-print(len(blocked))
+blocked = PreProcess.blocked_reactions(model)
+print(blocked)
