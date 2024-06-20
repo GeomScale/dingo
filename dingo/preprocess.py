3c51f8644634060077d043e91afe35834ef0e9ab
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2024-06-20 14:54:02 +0300

    added blocked, zero-flux and merge of removed reactions

--- dingo/preprocess.py
+++ dingo/preprocess.py
@@ -8,11 +8,29 @@ class PreProcess:
     def __init__(self, model):
         self.model = model
         
+        
     def objective_function(model):
         
         objective = str(model.summary()._objective)
         objective = objective.split(" ")[1]
         return objective
+    
+
+    def zero_flux(model):
+        
+        tol = 1e-6
+        
+        fva = cobra.flux_analysis.flux_variability_analysis(model, fraction_of_optimum=0)
+        zero_flux = fva.loc[ (abs(fva['minimum']) < tol ) & (abs(fva['maximum']) < tol)]
+        zero_flux = zero_flux.index.tolist()
+        
+        return zero_flux
+    
+    
+    def blocked(model):
+        
+        return cobra.flux_analysis.find_blocked_reactions(model)
+    
 
     def metabolically_less_efficient(model):
         
@@ -23,17 +41,35 @@ class PreProcess:
         model.objective = objective
         fba_solution = model.optimize()
 
+        wt_lower_bound = model.reactions.get_by_id(objective).lower_bound
         model.reactions.get_by_id(objective).lower_bound = fba_solution.objective_value
 
         fva = cobra.flux_analysis.flux_variability_analysis(model, fraction_of_optimum=0.95)
-        blocked_fva = fva.loc[ (abs(fva['minimum']) < tol ) & (abs(fva['maximum']) < tol)]
-        mle = blocked_fva.index.tolist()
+        mle = fva.loc[ (abs(fva['minimum']) < tol ) & (abs(fva['maximum']) < tol)]
+        mle = mle.index.tolist()
+        
+        model.reactions.get_by_id(objective).lower_bound = wt_lower_bound
         
         return mle
+    
+    
+    def remove_from_model(model):
+
+        remove_reactions = []
+
+        blocked = PreProcess.blocked(model)
+        mle = PreProcess.metabolically_less_efficient(model)
+        zero_flux = PreProcess.zero_flux(model)
+
+        remove_reactions = blocked+mle+zero_flux
+        remove_reactions_unique = list(set(remove_reactions))
+        
+        return remove_reactions_unique
+        
 
 
 model = load_json_model("../ext_data/e_coli_core.json")
 
-blocked = PreProcess.metabolically_less_efficient(model)
-print(blocked)
+remove = PreProcess.remove_from_model(model)
+print(remove)
 
