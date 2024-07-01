4b518df5e465bfffaad192e6a554523de8cef8bd
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2024-07-01 12:37:39 +0300

    minor fixes

--- tests/preprocess.py
+++ tests/preprocess.py
@@ -5,15 +5,13 @@ from dingo.preprocess import PreProcess
 
 class TestFba(unittest.TestCase):
 
-    def test_fba_json(self):
+    def test_preprocess(self):
 
-        #input_file_json = os.getcwd() + "/ext_data/e_coli_core.json"
+        input_file_json = os.getcwd() + "/ext_data/e_coli_core.json"
         PreProcess.blocked(input_file_json)
         #model = MetabolicNetwork.from_json(input_file_json)
-        #model.set_slow_mode()
-        #res = model.fba()
 
-        #self.assertTrue(abs(res[1] - 0.8739215067486387) < 1e-03)
+        #self.assertTrue()
         
         
 
