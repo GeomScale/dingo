811a33f9a2cb3f127be930326bb79ed94896d751
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2024-07-02 19:21:11 +0300

    simplified functions

--- dingo/preprocess.py
+++ dingo/preprocess.py
@@ -10,23 +10,55 @@ class PreProcess:
     def __init__(self, model):
         self.model = model
         self.objective = self.objective_function()
+        self.initial_reactions = self.initial()
+        self.essential_reactions = self.essentials()
         self.zero_flux_reactions = self.zero_flux()
         self.blocked_reactions = self.blocked()
         self.mle_reactions = self.metabolically_less_efficient()
-        self.removed_reactions = self.list_removed_reactions()
-        self.essential_reactions = self.possible_essential_reactions()
-        
+        self.removed_reactions = []
+
+
         
     def objective_function(self):
+        """
+        A function  used to find the objective function of a model
+        """
         
         objective = str(self.model.summary()._objective)
         objective = objective.split(" ")[1]
         
         self.objective = objective
         return self.objective
+
+
+    def initial(self):
+        
+        self.initial_reactions = []
+        
+        for reaction in self.model.reactions:
+            reaction_id = reaction.id
+            self.initial_reactions.append(reaction_id)
+            
+        return self.initial_reactions
+
+
+    def essentials(self):
+        
+        self.essential_reactions = []
+        essential_reactions = cobra.flux_analysis.find_essential_reactions(self.model)
+        for reaction in essential_reactions:
+            reaction_id = reaction.id
+            self.essential_reactions.append(reaction_id)
+            
+        return self.essential_reactions
     
 
     def zero_flux(self):
+        """
+        A function used to find zero-flux reactions.
+        These reactions are the ones that have a flux equaled to 0
+        when running FVA analysis with fraction of optimum set to 90%
+        """
         
         tol = 1e-6
         
@@ -39,6 +71,10 @@ class PreProcess:
     
     
     def blocked(self):
+        """
+        A function used to find blocked reactions.
+        These reactions can not have any flux other than 0
+        """
         
         blocked_reactions = cobra.flux_analysis.find_blocked_reactions(self.model)
         self.blocked_reactions =  blocked_reactions
@@ -66,17 +102,6 @@ class PreProcess:
         return self.mle_reactions
     
     
-    def list_removed_reactions(self):
-
-        remove_reactions = []
-
-        remove_reactions = self.blocked_reactions + self.mle_reactions + self.zero_flux_reactions
-        list_removed_reactions = list(set(remove_reactions))
-        
-        self.removed_reactions = list_removed_reactions
-        return self.removed_reactions
-        
-
     def remove_model_reactions(self):
                         
         for reaction in self.removed_reactions:
@@ -84,67 +109,54 @@ class PreProcess:
             self.model.reactions.get_by_id(reaction).upper_bound = 0
             
         return self.model
+    
             
-
-    def possible_essential_reactions(self):
-            
-        # find model reactions
-        reactions_list = []
+    def removed_reactions_ids(self):
         
-        for reaction in self.model.reactions:
-            reaction_id = reaction.id
-            reactions_list.append(reaction_id)
-            
-        remained_reactions = list((Counter(reactions_list)-Counter(self.removed_reactions)).elements())
+        #remove_reactions = []
+        blocked_mle_zero = self.blocked_reactions + self.mle_reactions + self.zero_flux_reactions
+        list_removed_reactions = list(set(blocked_mle_zero))
+        
+        self.removed_reactions = list_removed_reactions
    
-        # find essential reactions
-        essential_reactions_list = []
-        essential_reactions = cobra.flux_analysis.find_essential_reactions(self.model)
-        for reaction in essential_reactions:
-            reaction_id = reaction.id
-            essential_reactions_list.append(reaction_id)
-            
-        possible_essential = list((Counter(remained_reactions)-Counter(essential_reactions_list)).elements())
-        
         
         self.remove_model_reactions()
         
-        
-        for reaction in possible_essential:
+        remained_reactions = list((Counter(self.initial_reactions)-Counter(self.removed_reactions)).elements())
+        remained_reactions = list((Counter(remained_reactions)-Counter(self.essential_reactions)).elements())
+   
+        for reaction in remained_reactions:
                         
             initial_lower = self.model.reactions.get_by_id(reaction).lower_bound
             initial_upper = self.model.reactions.get_by_id(reaction).upper_bound
             
-            fba_solution_before = self.model.optimize().objective_value
-
-            self.model.reactions.get_by_id(reaction).lower_bound = 0.01 * initial_lower
-            self.model.reactions.get_by_id(reaction).upper_bound = 0.01 * initial_upper
+            self.model.reactions.get_by_id(reaction).lower_bound = 0
+            self.model.reactions.get_by_id(reaction).upper_bound = 0
     
             fba_solution_after = self.model.optimize().objective_value
-                                   
-            if fba_solution_after < fba_solution_before:
-                if abs(fba_solution_before)-abs(fba_solution_after) > 0.3:
-                    essential_reactions_list.append(reaction)
-                    
-                    self.model.reactions.get_by_id(reaction).upper_bound = initial_upper
-                    self.model.reactions.get_by_id(reaction).lower_bound = initial_lower
+            if (fba_solution_after == None) | (fba_solution_after == 0.0):
+                pass
+            else:
+                self.removed_reactions.append(reaction)
+              
+            self.model.reactions.get_by_id(reaction).upper_bound = initial_upper
+            self.model.reactions.get_by_id(reaction).lower_bound = initial_lower
             
             
-        self.essential_reactions = essential_reactions_list
-        return self.essential_reactions
+        self.remove_model_reactions()
+        print(len(self.removed_reactions), "of the", len(self.initial_reactions), "reactions were removed from the model")
+        return self.removed_reactions
+     
         
-        
-if __name__ == "__main__":
-    PreProcess()    
 
-#model = load_json_model("ext_data/e_coli_core.json")
+model = load_json_model("ext_data/e_coli_core.json")
 
 #fba_solution = model.optimize()
 #print(fba_solution.objective_value)
 
-#obj = preprocess(model)
-
-#print(len(obj.essential_reactions))
+obj = PreProcess(model)
+rem = obj.removed_reactions_ids()
+print(len(rem))
 
 #fba_solution = model.optimize()
 #print(fba_solution.objective_value)
